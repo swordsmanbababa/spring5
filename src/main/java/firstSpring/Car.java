b0112a654f04be830521f8b8da5d668edb1758fe
swordsmanbababa <773709579@qq.com>
2020-09-29 20:56:17 +0800

    dependency

---
+++ src/main/java/firstSpring/Car.java
@@ -0,0 +1,4 @@
+package firstSpring;
+
+public class Car {
+}
--- src/main/java/firstSpring/Person.java
+++ src/main/java/firstSpring/Person.java
@@ -1,8 +1,79 @@
 package firstSpring;
 
+
+import java.util.*;
+
 public class Person {
     Integer id;
     String name;
+    String [] emails;
+    Set<String> tels;
+    List<String> address;
+    Map<String,String> qqs;
+    Properties p;
+    Car car;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", emails=" + Arrays.toString(emails) +
+                ", tels=" + tels +
+                ", address=" + address +
+                ", qqs=" + qqs +
+                ", p=" + p +
+                ", car=" + car +
+                '}';
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Properties getP() {
+        return p;
+    }
+
+    public void setP(Properties p) {
+        this.p = p;
+    }
+
+    public Map<String, String> getQqs() {
+        return qqs;
+    }
+
+    public void setQqs(Map<String, String> qqs) {
+        this.qqs = qqs;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
+    public Set<String> getTels() {
+        return tels;
+    }
+
+    public void setTels(Set<String> tels) {
+        this.tels = tels;
+    }
+
+    public String[] getEmails() {
+        return emails;
+    }
+
+    public void setEmails(String[] emails) {
+        this.emails = emails;
+    }
 
     public Integer getId() {
         return id;
@@ -20,11 +91,4 @@ public class Person {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
